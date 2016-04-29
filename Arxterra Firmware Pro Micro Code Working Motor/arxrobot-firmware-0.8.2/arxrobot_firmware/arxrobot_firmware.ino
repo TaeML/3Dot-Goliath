af2494199439d160b16a8025e78157e6f06dca27
TaeML <taelee7657@yahoo.com>
2016-04-28 21:52:13 -0700

    Added files via upload

---
+++ Arxterra Firmware Pro Micro Code Working Motor/arxrobot-firmware-0.8.2/arxrobot_firmware/arxrobot_firmware.ino
@@ -0,0 +1,143 @@
+/* The following two libraries are included with the Arduino IDE  */
+#include <SPI.h>       // supports SPI serial communication and defines analog pins
+#include <EEPROM.h>
+#include <Wire.h>      // I2C support (currently not implemented)
+//#include <L3G4200D.h>  // 3-axis Gyro (currently not implemented)
+
+#include "pitches.h"
+
+#define FALSE 0        // LOW
+#define TRUE  1        // HIGH
+
+/**************** Robot Configuration **************
+ * Set Rover, Pinouts, Connection Type, and Pinger *
+ ***************************************************/
+#include "pinouts_robot.h"    // replace with your pinout
+
+
+
+#define Rosco FALSE
+#define PaperBot TRUE
+#define bluetooth FALSE       // Leonardo class rovers - Serial used for USB, Serial1 for bluetooth (i.e., USART)
+#define pinger FALSE          // FALSE if no ultrasonic sensor is used.
+#define debug  FALSE
+#define blinkled TRUE
+
+unsigned long ping_interval = 5000;  // 5 seconds
+unsigned long current = millis();
+unsigned long endState = current + ping_interval;
+// note: #define is a text substitution pre-processor directive
+ 
+/**************** Global Variables ****************
+ * source: Found in CommunicationRobotPilot Folder  *
+ ***************************************************/
+const int16_t FLAG = -128;  // value returned by readFuelGauge when called
+                            // by sendDataon undervoltage condition
+// Timer Variable
+unsigned long timer;        // unsigned 32-bit
+boolean tagged = FALSE;
+unsigned int count = 0;
+int16_t temp_value = 0x005;
+
+
+boolean collisionDetection = FALSE;
+ 
+void setup(){
+  Serial.begin(9600);
+  Serial1.begin(9600);      // legacy rovers operated at 57600
+  #if bluetooth
+  Serial.begin(9600);
+  #endif
+
+  
+  //LEDinit();
+  /*
+  while (!Serial) {
+    ; // wait for USB serial port to connect. Needed for Leonardo only
+  }
+  */
+  
+  timer = millis();        // telemetry  
+  // init_servos();        // Rosco uses servos
+  
+  pinMode(led, OUTPUT);    // initialize LED indicator as an output.
+ 
+  // Configure motorshield and insure motors are off. 
+  // If included in design, turn off collisionDetection
+  setup_TB6612FNG();    // sparkfun_TB6612FNG
+}
+
+void loop()
+{
+  
+  #if bluetooth
+  if(Serial.available() ) commandDecoder();  // note: Leonardo does not support serialEvent() handler
+  #else
+  if(Serial1.available() ) commandDecoder();
+  #endif
+
+/*
+  if (tagged == FALSE)
+  {
+    //Recieve value from emitter
+    tagged = TRUE;
+   // tone(3,NOTE_C5);
+    count = count + 1;
+  }
+
+  if (current == endState)&&(tagged == TRUE)&&(count == 3)
+  {
+    count = 0;
+    tagged = FALSE;
+    sendData();     // Call to send Data for Control Panel Feedback
+
+    if(temp_value == 85)
+    {
+    temp_value = 0x0005;
+    }
+  }
+  */
+  
+  // future: replace with watchdog timer interrupt  ****
+
+  /*
+  if (millis() > getNextPing()){   
+    sendWordPacket(EMERGENCY_ID,WATCHDOG_TIMEOUT);
+    
+    #if bluetooth                                // if packet sent over USART=>bluetooth,  
+      Serial.print("Emergency exception 0x0");   // send duplicate data as text to
+      Serial.println(WATCHDOG_TIMEOUT,HEX);      // USB=>Arduino IDE Serial Monitor.
+    #endif 
+    */
+    // ****** SLEEP PAPERBOT *******
+    
+    // ##### JEFF CHANGED
+    // Until the above-mentioned "SLEEP PAPERBOT" is implemented,
+    // let's reset to allow another ping interval to avoid the
+    // constant barrage of an exception message on every loop.
+  //  updateNextPing();
+    // #####
+  
+
+  
+ 
+
+  // If using a Pinger detect a collision
+  /*
+  #if pinger == TRUE
+      if (collisionDetection)
+      {
+        if (checkSonar()) 
+        {
+          // insure motors are off and turn off collisionDetection
+          stopMotors();
+          collisionDetection = FALSE;   // rover is not moving
+          // send Emergency Stop message
+        }
+      }
+  #endif
+  */
+
+  
+  
+}  // end of loop
