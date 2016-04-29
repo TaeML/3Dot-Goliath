79399b7fc7802000a0e9392f8e925232f4212003
TaeML <taelee7657@yahoo.com>
2016-04-28 21:51:29 -0700

    Added files via upload

---
+++ PCB_Test/PCB_Test.ino
@@ -0,0 +1,23 @@
+int sum = 0;
+int sample_count = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  /*
+  while (sample_count < 10) 
+  {
+    sum += analogRead(A5);
+    sample_count++;
+    delay(500);
+  }*/
+  int sensorValue = digitalRead(2);
+  // put your main code here, to run repeatedly:
+  float voltage = sensorValue;
+  Serial.println(voltage);
+  //Serial.println(sensorValue);
+  delay(1000);
+}
